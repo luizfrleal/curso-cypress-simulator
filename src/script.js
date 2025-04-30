a6daf84a4e133dd9a251bd300997a63afc763a3a
luizfrleal <luizfrleal@gmail.com>
2025-04-30 09:59:14 -0300

    fix bug

--- src/script.js
+++ src/script.js
@@ -257,7 +257,7 @@ document.addEventListener("DOMContentLoaded", () => {
       mainContent.style.display = "flex"
       sandwichMenu.style.display = "flex"
       captchaError.style.display = "none"
-      captchaInput.value = ""
+      //captchaInput.value = ""
       verifyCaptchaButton.disabled = true
 
       checkCookieConsent()
